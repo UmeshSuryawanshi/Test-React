df70beb4018e492240ac7079a45fb1d6a3730152
Umesh Suryawanshi <usuryawanshi@avaya.com>
2024-03-18 01:00:22 +0530

    first commit from Front end

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const parent = React.createElement("div", { id: "parent" }, 
+[React.createElement("div", { id: "child1" }, [React.createElement("h1", {id:"h1"}, "I am H1 tag"), React.createElement("h2", {id:"h2"}, "I am H2 tag")]),
+React.createElement("div", { id: "child2" }, [React.createElement("h1", {id:"h1"}, "I am H1 tag"), React.createElement("h2", {id:"h2"}, "I am H2 tag")])]);
+
+console.log(parent);
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(parent);
